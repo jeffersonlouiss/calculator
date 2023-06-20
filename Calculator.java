e51a31f53a70ee20692360ff4ca06be14700d2e7
jeffersonLouis <jefferson.saleh@gmail.com>
2023-06-20 15:29:23 -0300

    save merged feature/addition

--- Calculator.java
+++ Calculator.java
@@ -1,4 +1,12 @@
 public class Calculator {
 	public static void main(String[] args) {}
 	
+	public int additon(int a, int b){
+		return a + b;
+	}
+
+	public double addition(double a, double b)
+	{
+		return a + b;
+	}
 }
